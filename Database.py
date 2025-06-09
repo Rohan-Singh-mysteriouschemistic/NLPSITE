ebd1f43aca3ce309f0fe7ef899ba804e22a211ad
Rohan-Singh-mysteriouschemistic <rohansingh2402.rdps@gmail.com>
2025-06-09 09:50:43 +0530

    Updating Data.json path

--- Database.py
+++ Database.py
@@ -1,23 +1,32 @@
+import os
 import json
 
 class DataBase:
+    def __init__(self):
+        self.data_file = os.path.join(os.path.dirname(__file__), "Data.json")
+
     def File_Register(self, Name, Email, Password):
-        with open(r"Data.json", "r") as F1:
-            data= json.load(F1)
+        if not os.path.exists(self.data_file):
+            with open(self.data_file, "w") as f:
+                json.dump({}, f)
+
+        with open(self.data_file, "r") as F1:
+            data = json.load(F1)
+
         if Email in data:
             return False
         else:
-            data[Email]=[Name, Password]
-            with open(r"Data.json", "w") as F1:
+            data[Email] = [Name, Password]
+            with open(self.data_file, "w") as F1:
                 json.dump(data, F1)
             return True
 
     def Check_Credentials(self, email, password):
-        with open(r"Data.json", "r") as F1:
+        with open(self.data_file, "r") as F1:
             data = json.load(F1)
+
         if email in data and data[email][1] == password:
             name = data[email][0]
             return {"name": name, "email": email}
         else:
             return None
-
